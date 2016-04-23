7c8b62411dc6958d4f186aeee545b0b5fc641aed
EQuimper <quimperemanuel@gmail.com>
2016-04-23 11:03:44 -0600

    Add README and some scripts

---
+++ README.md
@@ -0,0 +1,5 @@
+* git clone the repos
+* npm install (npm i)
+* bower install (bower i)
+* npm i -g webpack-dev-server (if not already install)
+* npm start
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const App = React.createClass({
   render() {
     return (
-      <div><h1>HELLO</h1></div>
+      <div className="col-lg-offset-4 col-lg-4">
+        <h1>HELLO</h1>
+	      <h1>Style me with <a href="http://flexboxgrid.com/" target="_blank">FlexboxGrid</a></h1>
+      </div>
     );
   }
 });
