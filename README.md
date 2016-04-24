46f1e452b18a48c4b8091e8caecf2c2748a4633e
EQuimper <quimperemanuel@gmail.com>
2016-04-23 20:10:02 -0600

    Change script

--- README.md
+++ README.md
@@ -11,6 +11,4 @@ If webpack-dev-server is not already install do
 ### If you want to change the remote for your own project just
 > git remote rm origin
 
-### You need to install concurrently for run npm start
-#### [concurrently Docs] (https://www.npmjs.com/package/concurrently)
-> npm i -g concurrently
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const App = React.createClass({
   render() {
     return (
-      <div className="col-lg-offset-4 col-lg-4">
-        <h1>HELLO</h1>
+      <div className="col-lg-offset-4 col-md-offset-4 col-lg-4 col-md-4">
+        <h1>Hello</h1>
 	      <h1>Style me with <a href="http://flexboxgrid.com/" target="_blank">FlexboxGrid</a></h1>
       </div>
     );
